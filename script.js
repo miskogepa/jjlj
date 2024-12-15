f1eee99c4b88855f230613133413edc1f4b29834
miskogepa <milovan.jankovic50m@gmail.com>
2024-12-15 20:01:25 +0100

    efe

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         alert('Invalid credentials');
     }
 });
+
+document.getElementById('dugme3').addEventListener('click', function() {
+    fetch('kk.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('chat-container').innerHTML = data;
+            document.getElementById('chat-container').style.display = 'block';
+            document.getElementById('card-container').style.display = 'none';
+        })
+        .catch(error => console.error('Error loading chat:', error));
+});
